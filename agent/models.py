0f448bba2407112fdfffe931ccba96ff79a205bd
Jonathan Tan <jontan@Jonathans-MacBook-Pro.local>
2021-10-08 21:51:28 +0800

    Add Obs & Act Space Modularity

--- agent/models.py
+++ agent/models.py
@@ -4,11 +4,11 @@ from tensorflow.python.keras.layers.convolutional import Conv
 
 # Various Neural Network Architectures are Defined Here
 
-def DoubleConv256():
+def DoubleConv256(obs_shape):
 
     model = Sequential()
 
-    model.add(Permute((3,2,1), input_shape=(4, 128, 128)))
+    model.add(Permute((3,2,1), input_shape=obs_shape))
     model.add(Conv2D(256,(3,3)))
     model.add(Activation("relu"))
     model.add(MaxPooling2D(2,2))
@@ -23,11 +23,11 @@ def DoubleConv256():
 
     return model
 
-def ConvLSTM():
+def ConvLSTM(obs_shape):
 
     model = Sequential()
 
-    model.add(Permute((3,2,1), input_shape=(4, 128, 128)))
+    model.add(Permute((3,2,1), input_shape=obs_shape))
     model.add(Conv2D(256,(3,3)))
     model.add(Activation("relu"))
     model.add(MaxPooling2D(2,2))
@@ -50,5 +50,5 @@ model_factory = {
     "ConvLSTM"      : ConvLSTM
 }
 
-def get_model(arch):
-    return model_factory[arch]()
+def get_model(opt):
+    return model_factory[opt.arch](opt.obs_dim)
